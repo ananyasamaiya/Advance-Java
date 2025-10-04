ac7e325978b1f7432020d93d6b275c46798ca4c9
ananyasamaiya <ananyasamaiya05@gmail.com>
2025-10-04 19:22:55 +0530

    First_servlet

---
+++ 5_first_servlet/WEB-INF/src/FirstServlet.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+@WebServlet("/act.do")
+public class FirstServlet extends HttpServlet{
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException,ServletException{
+        System.out.println("Hello World");
+    }
+}
