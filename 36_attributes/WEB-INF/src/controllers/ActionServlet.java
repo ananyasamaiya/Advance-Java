85b4ed54811d6ed73d7b7c397d27ff77b3383035
ananyasamaiya <ananyasamaiya05@gmail.com>
2025-11-05 14:54:05 +0530

    attributes

---
+++ 36_attributes/WEB-INF/src/controllers/ActionServlet.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+
+@WebServlet("/act.do")
+public class ActionServlet extends HttpServlet{
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        request.setAttribute("info","Sumit");
+
+
+        request.getRequestDispatcher("next.jsp").forward(request, response);
+    }
+}
+
