e5c993d8c6e819a7c6bafc87fe6897e578ba5fcf
ananyasamaiya <ananyasamaiya05@gmail.com>
2025-12-07 17:39:58 +0530

    Send Email

---
+++ 61_send_email/WEB-INF/src/controllers/SendEmailServlet.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+
+import utils.EmailSender;
+
+
+@WebServlet("/send_email.do")
+public class SendEmailServlet extends HttpServlet{
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        String email = request.getParameter("email");
+
+        EmailSender.sendEmail(email,"Another Email Sending text","Hello from Ananya Samaiya");
+
+        
+        request.getRequestDispatcher("index.html").forward(request,response);
+    }
+}
+
---
+++ 61_send_email/WEB-INF/src/utils/EmailSender.java
@@ -0,0 +1,51 @@
+package utils;
+
+import javax.mail.Session;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.internet.MimeMessage;
+import javax.mail.MessagingException;
+import javax.mail.Message;
+import javax.mail.Message.RecipientType;
+import javax.mail.Transport;
+
+import java.util.Properties;
+
+public class EmailSender {
+    public static void sendEmail(String to, String subject, String msg){
+        Properties props = new Properties();
+
+        // search gmail smtp to get host and port
+
+        // smtp - Simple Mail Transfer Protocol
+        props.put("mail.transport.protocol","smtp");
+        props.put("mail.smtp.host","smtp.gmail.com");
+        props.put("mail.smtp.port","587");
+        props.put("mail.smtp.auth","true");
+        props.put("mail.smtp.starttls.enable","true");
+
+        Session ssn = Session.getInstance(props,new MailAuthenticator());
+
+        // Mime full form - multipurpose internet mail extensions 
+
+        MimeMessage mm = new MimeMessage(ssn);
+        try{
+            // mm.setFrom("ISRCD EMAIL..."); exception
+            // mm.setFrom("ananya@gmail.com"); not send
+            mm.setFrom("samaiyaananya1@gmail.com");
+            mm.setRecipients(Message.RecipientType.TO,to);
+            mm.setSubject(subject);
+            mm.setText(msg);
+
+            Transport.send(mm);
+        }catch(MessagingException e){
+            e.printStackTrace();
+        }
+    }
+}
+
+class MailAuthenticator extends Authenticator{
+    public PasswordAuthentication getPasswordAuthentication(){
+        return new PasswordAuthentication("samaiyaananya1@gmail.com","sbwczzmrekydvmmo");
+    }
+}
